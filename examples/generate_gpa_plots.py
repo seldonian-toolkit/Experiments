a0428769014bd3f5db630cfdbf49a585d382bb9e
Austin Hoag <austinthomashoag@gmail.com>
2022-05-26 12:59:46 -0700

    create examples folder

---
+++ examples/generate_gpa_plots.py
@@ -0,0 +1,52 @@
+import os
+import autograd.numpy as np   # Thinly-wrapped version of Numpy
+
+from experiments.utils import generate_resampled_datasets
+from experiments.generate_plots import SupervisedPlotGenerator
+
+from seldonian.utils.io_utils import load_pickle
+
+def accuracy(model,theta,X,y):
+	""" Binary classification accuracy """
+	prediction = model.predict(theta,X)
+	predict_class = prediction>=0.5
+	acc = np.mean(1.0*predict_class==y)
+	return acc
+
+
+if __name__ == "__main__":
+	# Load spec
+	interface_output_dir = os.path.join('/Users/ahoag/beri/code',
+		'interface_outputs/demographic_parity_interface_withspec')
+	specfile = os.path.join(interface_output_dir,'spec.pkl')
+	spec = load_pickle(specfile)
+
+	### PARAMETER SETUP ###
+	n_trials = 10
+	data_pcts = np.hstack([[0.0004,0.0008],np.logspace(-3,0,15)])
+	# data_pcts = [0.001]
+	n_jobs=8
+	results_dir = './results/demographic_parity_fromspec'
+	verbose=True
+
+	os.makedirs(results_dir,exist_ok=True)
+
+	plot_generator = SupervisedPlotGenerator(
+		spec=spec,
+		n_trials=n_trials,
+		data_pcts=data_pcts,
+		n_jobs=n_jobs,
+		eval_method='resample',
+		perf_eval_fn=accuracy,
+		results_dir=results_dir,
+		)
+
+	plot_generator.run_seldonian_experiment(verbose=verbose)
+
+	savename = os.path.join(results_dir,'demographic_parity_withspec.png')
+	plot_generator.make_plots(fontsize=12,legend_fontsize=8,
+		performance_label='accuracy',best_performance=None,
+		savename=savename)
+
+
+
--- experiments/experiments.py
+++ experiments/experiments.py
@@ -6,6 +6,7 @@ from concurrent.futures import ProcessPoolExecutor
 import multiprocessing as mp
 from functools import partial
 from tqdm import tqdm
+import copy
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
@@ -13,9 +14,7 @@ from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.model_selection import train_test_split
 
 from seldonian.dataset import DataSet
-from seldonian.model import *
-from seldonian.candidate_selection import CandidateSelection
-from seldonian.safety_test import SafetyTest
+from seldonian import seldonian_algorithm
 
 class Experiment():
 	def _aggregate_results(self,**kwargs):
@@ -258,7 +257,7 @@ class SeldonianExperiment(Experiment):
 		self.model_name = 'qsa'
 
 	def run_experiment(self,**kwargs):
-		
+		n_jobs = kwargs['n_jobs']
 		partial_kwargs = {key:kwargs[key] for key in kwargs \
 			if key not in ['data_pcts','n_trials']}
 
@@ -271,44 +270,53 @@ class SeldonianExperiment(Experiment):
 		n_trials = kwargs['n_trials']
 		data_pcts_vector = np.array([x for x in data_pcts for y in range(n_trials)])
 		trials_vector = np.array([x for y in range(len(data_pcts)) for x in range(n_trials)])
-		# for ii in range(len(data_pcts_vector)):
-		# 	data_pct = data_pcts_vector[ii]
-		# 	trial_i = trials_vector[ii]
-		# 	print(data_pct,trial_i)
-		# 	helper(data_pct,trial_i)
 
-		with ProcessPoolExecutor(max_workers=kwargs['n_jobs'],mp_context=mp.get_context('fork')) as ex:
-			results = tqdm(ex.map(helper,data_pcts_vector,trials_vector),
-				total=len(data_pcts_vector))
-			for exc in results:
-				if exc:
-					print(exc)
+		if n_jobs == 1:
+			for ii in range(len(data_pcts_vector)):
+				data_pct = data_pcts_vector[ii]
+				trial_i = trials_vector[ii]
+				print(data_pct,trial_i)
+				helper(data_pct,trial_i)
+		elif n_jobs > 1:
+			with ProcessPoolExecutor(max_workers=n_jobs,
+				mp_context=mp.get_context('fork')) as ex:
+				results = tqdm(ex.map(helper,data_pcts_vector,trials_vector),
+					total=len(data_pcts_vector))
+				for exc in results:
+					if exc:
+						print(exc)
+		else:
+			raise ValueError(f"n_jobs value of {n_jobs} must be >=1 ")
+
 		self._aggregate_results(**kwargs)
 	
 	def QSA(self,data_pct,trial_i,**kwargs):
-		# try: 
+		
+		spec = kwargs['spec']
 		verbose=kwargs['verbose']
-		regime=kwargs['regime']
-		frac_data_in_safety = kwargs['frac_data_in_safety']
-		initializer=kwargs['initializer']
-		optimization_technique=kwargs['optimization_technique']
-		optimizer=kwargs['optimizer']
+		eval_method = kwargs['eval_method']
+		perf_eval_fn = kwargs['perf_eval_fn']
+
+		regime=spec.dataset.regime
 
 		trial_dir = os.path.join(
-				kwargs['results_dir'],
+				self.results_dir,
 				'qsa_results',
 				'trial_data')
+
 		savename = os.path.join(trial_dir,
 			f'data_pct_{data_pct:.4f}_trial_{trial_i}.csv')
+
 		if os.path.exists(savename):
-			print(f"Trial {trial_i} already run for"
-				   f"this data_pct: {data_pct}. Skipping this trial. ")
+			if verbose:
+				print(f"Trial {trial_i} already run for"
+					  f"this data_pct: {data_pct}. Skipping this trial. ")
 			return
 
 		os.makedirs(trial_dir,exist_ok=True)
 		
-		parse_trees = kwargs['parse_trees']
-		dataset = kwargs['dataset']
+		parse_trees = spec.parse_trees
+		dataset = spec.dataset
 
 		if regime == 'supervised':
 			# Load in ground truth
@@ -320,60 +328,42 @@ class SeldonianExperiment(Experiment):
 			include_intercept_term = dataset.include_intercept_term
 			label_column = dataset.label_column
 
-			if kwargs['eval_method'] == 'resample':
-				# resampled_filename = os.path.join(kwargs['results_dir'],
-				# 	'resampled_dataframes',f'trial_{trial_i}.csv')
-				resampled_filename = os.path.join(kwargs['results_dir'],
+			if eval_method == 'resample':
+				resampled_filename = os.path.join(self.results_dir,
 					'resampled_dataframes',f'trial_{trial_i}.pkl')
 				n_points = int(round(data_pct*len(test_features))) 
-				# df = pd.read_csv(resampled_filename).iloc[:n_points]
+
 				with open(resampled_filename,'rb') as infile:
 					resampled_df = pickle.load(infile).iloc[:n_points]
-				print(f"Using resampled dataset {resampled_filename} with {len(resampled_df)} datapoints")
+
+				if verbose:
+					print(f"Using resampled dataset {resampled_filename} "
+						  f"with {len(resampled_df)} datapoints")
 			else:
 				raise NotImplementedError(
-					f"Eval method {kwargs['eval_method']} "
-					"not supported for regime={regime}")
+					f"Eval method {eval_method} "
+					f"not supported for regime={regime}")
 
-			candidate_df, safety_df = train_test_split(
-				resampled_df, test_size=frac_data_in_safety, shuffle=False)
-
-			sensitive_candidate_df = candidate_df[sensitive_column_names]
-			sensitive_safety_df = safety_df[sensitive_column_names]
-			
-			candidate_dataset = DataSet(
-				candidate_df,meta_information=resampled_df.columns,
+			dataset_for_experiment = DataSet(
+				df=resampled_df,
+				meta_information=resampled_df.columns,
+				regime=regime,
+				label_column=label_column,
 				sensitive_column_names=sensitive_column_names,
 				include_sensitive_columns=include_sensitive_columns,
-				include_intercept_term=include_intercept_term,
-				regime=regime,label_column=label_column)
+				include_intercept_term=include_intercept_term)
 
-			safety_dataset = DataSet(
-				safety_df,meta_information=resampled_df.columns,
-				sensitive_column_names=sensitive_column_names,
-				include_sensitive_columns=include_sensitive_columns,
-				include_intercept_term=include_intercept_term,
-				regime=regime,label_column=label_column)
+			# Make a new spec object where the 
+			# only thing that is different is the dataset
 
-			n_safety = len(safety_df)
-			print(f"Candidate dataset of length: {len(candidate_df)}")
-			print(f"Safety dataset of length: {len(safety_df)}")
-			# Set up for initial solution
-			candidate_labels = candidate_dataset.df[label_column]
-			candidate_features = candidate_dataset.df.loc[:,
-				candidate_dataset.df.columns != label_column]
-			
-			if not include_sensitive_columns:
-				candidate_features = candidate_features.drop(
-					columns=candidate_dataset.sensitive_column_names)
-
-			if include_intercept_term:
-				candidate_features.insert(0,'offset',1.0) # inserts a column of 1's
+			spec_for_experiment = copy.deepcopy(spec)
+			spec_for_experiment.dataset = dataset_for_experiment
+			model_instance = spec_for_experiment.model()
 
 		elif regime == 'RL':
 			RL_environment_obj = kwargs['RL_environment_obj']
 			
-			if kwargs['eval_method'] == 'generate_episodes':
+			if eval_method == 'generate_episodes':
 				# Sample from resampled dataset on disk of n_episodes
 				save_dir = os.path.join(kwargs['results_dir'],'resampled_datasets')
 				# savename = os.path.join(save_dir,f'resampled_df_trial{trial_i}.csv')
@@ -412,7 +402,7 @@ class SeldonianExperiment(Experiment):
 				# print(candidate_df)
 			else:
 				raise NotImplementedError(
-					f"Eval method {kwargs['eval_method']} "
+					f"Eval method {eval_method} "
 					"not supported for regime={regime}")
 			
 			candidate_dataset = DataSet(
@@ -429,138 +419,40 @@ class SeldonianExperiment(Experiment):
 			print(f"Candidate dataset has {n_candidate} episodes")
 		
 
-		# Determine model to use
-		if kwargs['seldonian_model_type'] == 'linear_classifier':
-			model = LinearClassifierModel()
-
-		elif kwargs['seldonian_model_type'] == 'logistic_regression':
-			model = LogisticRegressionModel()
-
-		elif kwargs['seldonian_model_type'] == 'tabular_softmax':
-			model = TabularSoftmaxModel(RL_environment_obj)
-
-		elif kwargs['seldonian_model_type'] == 'linear_softmax':
-			model = LinearSoftmaxModel(RL_environment_obj)
-		
-		## Initial solution
-		if regime == 'supervised':
-			if initializer == 'random':
-				initial_solution = np.random.normal(0,1,candidate_features.shape[1])
-			elif initializer == 'zeros':
-				initial_solution = np.zeros(candidate_features.shape[1])
-			elif initializer == 'fit':
-				try:
-					initial_solution = model.fit(candidate_features,candidate_labels)
-				except ValueError:
-					# If there is not enough data for a fit then use random init
-					initial_solution = np.random.normal(0,1,candidate_features.shape[1])
-			else:
-				raise NotImplementedError(f"Initialization method: {initializer} not supported")
-		elif regime == 'RL':
-			initial_solution = RL_environment_obj.initial_weights
-
-		print(f"initial solution is: {initial_solution}")
-
-		# Candidate selection
-		if kwargs['primary_objective'] == 'default':
-			primary_objective = model.default_objective
-		else:
-			primary_objective_str = kwargs['primary_objective']
-			if primary_objective_str == 'perceptron_loss':
-				primary_objective = model.sample_perceptron_loss
-			elif primary_objective_str == 'logistic_loss':
-				primary_objective = model.sample_logistic_loss
-
-		cs_kwargs = dict(
-			model=model,
-			candidate_dataset=candidate_dataset,
-			n_safety=n_safety,
-			parse_trees=parse_trees,
-			primary_objective=primary_objective,
-			optimization_technique=optimization_technique,
-			optimizer=optimizer,
-			initial_solution=initial_solution,
-			regime=regime,
-			write_logfile=True)
-		
-		if regime == 'RL':
-			cs_kwargs['gamma'] = RL_environment_obj.gamma
-			cs_kwargs['min_return'] = RL_environment_obj.min_return
-			cs_kwargs['max_return'] = RL_environment_obj.max_return
-
-		if 'reg_coef' in kwargs:
-			cs_kwargs['reg_coef'] = kwargs['reg_coef']
-
-		cs = CandidateSelection(**cs_kwargs)
-
-		minimizer_options = {'maxiter':kwargs['max_iter']}
-		cs_run_kwargs = dict(
-			minimizer_options=minimizer_options,
-			verbose=kwargs['verbose']
-		)
-		
-		print("Running candidate selection")
-		print()
-
-		if optimization_technique == 'gradient_descent':
-			cs_run_kwargs['use_primary_gradient'] = kwargs['use_primary_gradient']
-
-		candidate_solution = cs.run(**cs_run_kwargs)
-
-		# print()
-		# print("################################################")
-		# print("USING HARDCODED CANDIDATE SOLUTION FOR TESTING!!!")
-		# print("################################################")
-		# candidate_solution = np.array([-0.11979351, -0.09333089,  0.14412426,  0.13925383,  0.09697775,  0.03665841,
-		# 	0.38079478,  0.31782679, -0.09528776, -0.02311008])
-		# candidate_solution = np.array([-0.07964954,  0.09430439,  0.13646698,  0.09961439,  0.08887494, -0.00242865,
-  # 0.34886347,  0.24144642, -0.221433,   -0.02403533,])
-		print("Candidate solution:")
-		print(candidate_solution)
-		NSF = False
+		# Run Seldonian algorithm with experimental spec
+		passed_safety,candidate_solution = seldonian_algorithm(
+			spec_for_experiment)
+		print("Ran seldonian algorithm")
+		NSF=False
+		# Handle NSF 
 		if type(candidate_solution) == str and candidate_solution == 'NSF':
-			passed_safety=False
 			NSF = True
-		else:
-			# Safety test
-			st_kwargs = dict(dataset=safety_dataset,
-				model=model,
-				parse_trees=parse_trees,
-				regime=regime
-				)
-
-			if regime == 'RL':
-				st_kwargs['gamma'] = RL_environment_obj.gamma
-				st_kwargs['min_return'] = RL_environment_obj.min_return
-				st_kwargs['max_return'] = RL_environment_obj.max_return
-
-			st = SafetyTest(**st_kwargs)
-
-			passed_safety = st.run(candidate_solution,
-				bound_method='ttest')
-
-		failed=False # we said we passed the safety test but
-		# the constraint fails on the original dataset (ground truth)
+		
+		# Calculate peformance
+		
+		failed=False # flag for whether we were actually safe 
+		# on ground truth. Only relevant if we passed the safety test
 
 		if NSF:
-			print("NSF")
 			performance = -99.0
 		else:
 			# If passed the safety test, calculate performance
 			# using candidate solution 
 			if passed_safety:
-				print("Passed safety test. Calculating performance")
+				if verbose:
+					print("Passed safety test. Calculating performance")
 				if regime == 'supervised':
-					print("Calculating performance with candidate solution:")
-					print(candidate_solution)
+					if verbose: 
+						print("Calculating performance with candidate solution:")
+						print(candidate_solution)
 					
-					prediction_test = model.predict(
-						candidate_solution,
-						test_features)
-					predict_class = prediction_test>=0.5
-					acc = np.mean(1.0*predict_class==test_labels)
-					performance = acc
-					print(f"Accuracy = {performance}")
+					performance = perf_eval_fn(
+						model_instance,
+						candidate_solution,test_features,
+						test_labels)
+					
+					if verbose:
+						print(f"Performance = {performance}")
 				
 				elif regime == 'RL':
 					# Calculate J, the expected sum of discounted rewards
@@ -579,20 +471,26 @@ class SeldonianExperiment(Experiment):
 					print(f"Performance is J={performance}")
 				
 				# Calculate whether we failed on test set
-		
-	
-				print("Determining whether solution is actually safe on ground truth")
-				
+				if verbose:
+					print("Determining whether solution is actually safe on ground truth")
 				
 				if regime == 'supervised':
-					for parse_tree in parse_trees:
-						parse_tree.evaluate_constraint(theta=candidate_solution,
-							dataset=dataset,
-							model=model,regime='supervised',
+					for parse_tree in spec_for_experiment.parse_trees:
+						parse_tree.evaluate_constraint(
+							theta=candidate_solution,
+							dataset=spec_for_experiment.dataset,
+							model=model_instance,
+							regime='supervised',
 							branch='safety_test')
+						
 						ghat = parse_tree.root.value
 						if ghat > 0:
 							failed = True
+
+						if verbose:
+							constraint_str = parse_tree.constraint_str
+							print(f"ghat for constraint: {constraint_str}"
+								  f" on test set: {ghat}")
 
 				elif regime == 'RL':
 					ghat = gfunc(
@@ -601,20 +499,22 @@ class SeldonianExperiment(Experiment):
 						)
 					if ghat > 0:
 						failed = True
-				print(f"ghat on test set: {ghat}")
 				
-				if failed:
-					print("Solution was not actually safe on test set!")
-				else:
-					print("Solution was safe on test set")
-				print()
+				if verbose:
+					if failed:
+						print("Solution was not actually safe on test set!")
+					else:
+						print("Solution was safe on test set")
+					print()
 			else:
-				print("Failed safety test ")
-				performance = -99.0
+				if verbose:
+					print("Failed safety test ")
+					performance = -99.0
 		
 		# Reset param weights to their initial weights -- might not be neccesary?
 		if regime == 'RL':
 			RL_environment_obj.param_weights = RL_environment_obj.initial_weights
+
 		# Write out file for this data_pct,trial_i combo
 		data = [data_pct,
 			trial_i,
